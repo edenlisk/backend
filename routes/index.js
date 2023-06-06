97759b09607f776532ef73e1d6c088a308a6f28a
edenlisk <nkumbuyenock@gmail.com>
2023-06-06 22:33:05 +0200

    prepare index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res
+      .status(200)
+      .json(
+          {
+            status: "success",
+            data: {
+              id: 0,
+              name: "bituri fabrice",
+              username: "fabrice",
+              occupation: "software engineer"
+            }
+          }
+      )
 });
 
 module.exports = router;
